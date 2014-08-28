93388af7ceeca7bbbebcbb14ce72d0cab218b153
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2014-08-28 15:32:06 +0200

    Spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,30 @@ require 'lstash'
 
 require 'rspec/its'
 
+require 'timecop'
+
+ENV['ES_URL'] = nil
+
 RSpec.configure do |config|
   config.order = 'random'
+end
+
+require 'stringio'
+
+def capture_stdout(&blk)
+  old = $stdout
+  $stdout = fake = StringIO.new
+  blk.call
+  fake.string
+ensure
+  $stdout = old
+end
+
+def capture_stderr(&blk)
+  old = $stderr
+  $stderr = fake = StringIO.new
+  blk.call
+  fake.string
+ensure
+  $stderr = old
 end
