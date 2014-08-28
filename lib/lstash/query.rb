9329a6a615021c8bc3d4049462e96f8f803fd170
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2014-08-28 15:31:31 +0200

    Spec and implementation for Lstash::Query

---
+++ lib/lstash/query.rb
@@ -0,0 +1,159 @@
+require 'time'
+require 'date'
+require 'ostruct'
+
+module Lstash
+
+  class Query
+
+    class FormatError < StandardError; end
+    class QueryMissing < StandardError; end
+
+    LOGSTASH_PREFIX = 'logstash-'
+    WILDCARD_QUERY = '*'
+
+    def initialize(query = nil, arguments = {})
+      @query = query
+
+      @anchor = time_parse(arguments[:anchor], 'today')
+      @from   = time_parse(arguments[:from],   'today')
+      @to     = time_parse(arguments[:to],     'now')
+
+      @to = Time.now if @to > Time.now # prevent accessing non-existing times / indices
+    end
+
+    def time_range
+      OpenStruct.new(from: @from, to: @to)
+    end
+
+    def date_range
+      (@from.utc.to_date .. @to.utc.to_date)
+    end
+
+    def indices
+      date_range.map { |d| "#{LOGSTASH_PREFIX}#{d.strftime('%Y.%m.%d')}" }
+    end
+
+    def body
+      {
+        sort: sort_order,
+
+        # return in order of ascending timestamp
+        query: {
+          filtered: {
+            query:  es_query,
+            filter: es_filter
+          }
+        }
+      }
+    end
+
+    private
+
+    def time_parse(time_string, default)
+      time_string = time_string.strip rescue nil
+      time_string ||= default
+      case time_string
+      when 'firstday'
+        midnight_at_beginning_of_month
+      when 'now'
+        Time.now
+      when 'today'
+        midnight_today
+      when 'yesterday'
+        midnight_yesterday
+      else
+        Time.parse(time_string)
+      end
+    rescue ArgumentError
+      raise FormatError, "Invalid time format: #{time_string}"
+    end
+
+    def query
+      q = @query.dup.strip rescue ''
+      q = WILDCARD_QUERY if q.empty?
+      q
+    end
+
+    def sort_order
+      [ { '@timestamp' => { order: 'asc' } } ]
+    end
+
+    def es_query
+      {
+        bool: {
+          should: [
+            {
+              query_string: {
+                query: query
+              }
+            }
+          ]
+        }
+      }
+    end
+
+    def es_filter
+      {
+        bool: {
+          must: [
+            range: {
+              '@timestamp' => {
+                from: to_msec(time_range.from),
+                to:   to_msec(time_range.to)
+              }
+            },
+            # fquery: {
+            #   query: {
+            #     query_string: {
+            #       query: query
+            #     }
+            #   }
+            # }
+          ],
+          # must_not: [
+          #   fquery: {
+          #     query: {
+          #       query_string: {
+          #         query: query
+          #       }
+          #     }
+          #   }
+          # ],
+          # should: [
+          #   fquery: {
+          #     query: {
+          #       query_string: {
+          #         query: query
+          #       }
+          #     }
+          #   }
+          # ]
+        }
+      }
+    end
+
+    # Return the date of the first day of date's month
+    def midnight_at_beginning_of_month
+      Date.new(anchor_time.year, anchor_time.month, 1).to_time
+    end
+
+    def midnight_today
+      Date.today.to_time
+    end
+
+    def midnight_yesterday
+      (Date.today-1).to_time
+    end
+
+    def anchor_time
+      @anchor
+    end
+
+    def to_msec(time)
+      time.to_i * 1000
+    end
+
+  end
+
+end
---
+++ spec/lstash/query_spec.rb
@@ -0,0 +1,145 @@
+require 'spec_helper'
+require 'lstash/query'
+
+describe Lstash::Query do
+
+  context "running on 2014-08-03" do
+    let(:time)  { '2014-08-03 15:54:33' }
+    let(:query) { nil }
+    let(:options) { {} }
+
+    subject { Lstash::Query.new(query, options) }
+
+    before { Timecop.freeze(Time.parse(time)) }
+    after  { Timecop.return }
+
+    it { should_not be nil }
+    # it "should initialize properly" do
+    #   expect(subject).not_to be nil
+    # end
+
+    its('time_range.from') { should eq Time.parse('2014-08-03 00:00:00.000000000 +0200') }
+    its('time_range.to')   { should eq Time.parse('2014-08-03 15:54:33.000000000 +0200') }
+
+    its(:date_range) { should eq (Date.parse('2014-08-02')..Date.parse('2014-08-03')) }
+
+    its(:indices) { should eq [ "logstash-2014.08.02", "logstash-2014.08.03" ] }
+
+    context "with specific query" do
+      let(:query) { 'program:haproxy' }
+      its(:query) { should eq query }
+    end
+
+    context "without query" do
+      let(:query) { nil }
+      its(:query) { should eql '*' }
+    end
+
+    context "from 'yesterday'" do
+      let(:options) { { from: 'yesterday' }}
+      its('time_range.from') { should eq Time.parse('2014-08-02 00:00:00.000000000 +0200') }
+    end
+
+    context "from 'today'" do
+      let(:options) { { from: 'today' }}
+      its('time_range.from') { should eq Time.parse('2014-08-03 00:00:00.000000000 +0200') }
+    end
+
+    context "from 'now'" do
+      let(:options) { { from: 'now' }}
+      its('time_range.from') { should eq Time.parse('2014-08-03 15:54:33.000000000 +0200') }
+    end
+
+    context "to 'yesterday'" do
+      let(:options) { { to: 'yesterday' }}
+      its('time_range.to') { should eq Time.parse('2014-08-02 00:00:00.000000000 +0200') }
+    end
+
+    context "to 'today'" do
+      let(:options) { { to: 'today' }}
+      its('time_range.to') { should eq Time.parse('2014-08-03 00:00:00.000000000 +0200') }
+    end
+
+    context "to 'now'" do
+      let(:options) { { to: 'now' }}
+      its('time_range.to') { should eq Time.parse('2014-08-03 15:54:33.000000000 +0200') }
+    end
+
+    context "from 'firstday'" do
+
+      let(:options) { { from: 'firstday' } }
+      its('time_range.from') { should eq Time.parse('2014-08-01 00:00:00.000000000 +0200') }
+
+      context "anchor 'yesterday'" do
+        let(:anchor) { 'yesterday' }
+        its('time_range.from') { should eq Time.parse('2014-08-01 00:00:00.000000000 +0200') }
+      end
+
+      context "anchor 'today'" do
+        let(:anchor) { 'today' }
+        its('time_range.from') { should eq Time.parse('2014-08-01 00:00:00.000000000 +0200') }
+      end
+
+      context "anchor '2014-07-17'" do
+        let(:options) { { from: 'firstday', anchor: '2014-07-17' } }
+        its('time_range.from') { should eq Time.parse('2014-07-01 00:00:00.000000000 +0200') }
+      end
+
+      context "date range" do
+        let(:options) { { from: 'firstday', anchor: 'yesterday' } }
+        its(:date_range) { should eq (Date.parse('2014-07-31')..Date.parse('2014-08-03')) }
+      end
+
+      context "indices" do
+        let(:options) { { from: 'firstday', anchor: 'yesterday' } }
+        its(:indices) {
+          should eq [
+            "logstash-2014.07.31",
+            "logstash-2014.08.01",
+            "logstash-2014.08.02",
+            "logstash-2014.08.03",
+          ]
+        }
+      end
+
+    end
+
+    context "body" do
+      its(:body) { should eq ({
+        :sort => [{"@timestamp"=>{:order=>"asc"}}],
+        :query => {:filtered=>{
+          :query => { :bool => { :should => [ { :query_string => { :query=>"*" }}]}},
+          :filter=> { :bool => { :must   => [ { :range => { "@timestamp" => { :from => 1407016800000, :to => 1407074073000}}}]}}}}
+      })}
+    end
+
+  end
+
+  context "running on 2014-08-01" do
+    let(:time)  { '2014-08-01 12:53:03' }
+    let(:query) { nil }
+    let(:options) { {} }
+
+    subject { Lstash::Query.new(query, options) }
+
+    before { Timecop.freeze(Time.parse(time)) }
+    after  { Timecop.return }
+
+    context "from 'firstday' with 'yesterday' anchor" do
+      let(:options) { { anchor: 'yesterday', from: 'firstday' } }
+
+        its('time_range.from') { should eq Time.parse('2014-07-01 00:00:00.000000000 +0200') }
+        its('time_range.to')   { should eq Time.parse('2014-08-01 12:53:03.000000000 +0200') }
+    end
+
+    context "from 'firstday' with default 'today' anchor" do
+      let(:options) { { from: 'firstday', to: 'now' } }
+
+        its('time_range.from') { should eq Time.parse('2014-08-01 00:00:00.000000000 +0200') }
+        its('time_range.to')   { should eq Time.parse('2014-08-01 12:53:03.000000000 +0200') }
+    end
+
+  end
+
+end
+
